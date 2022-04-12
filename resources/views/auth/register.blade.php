2d6d86e80be25844288e78b42cd0d3cea8021705
hntake <hntake@gmail.com>
2022-04-12 11:23:10 +0900

    logout marign

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('登録画面') }}</div>
+                <div class="card-header" style="font-weight:bold;margin-bottom: 20px;">{{ __('登録画面') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
